ce440db6c5d972a3700d2277bea89af3d6e54163
evwicke <evanwicke@icloud.com>
2025-09-11 10:43:28 -0400

    fix: correct main method signature and expand variable initialization in OperationAndNewDataTypes.java

--- In Class Assignments/src/OperationAndNewDataTypes.java
+++ In Class Assignments/src/OperationAndNewDataTypes.java
@@ -6,12 +6,32 @@
 
  */
 
-import java.util.Scanner;
-
 public class OperationAndNewDataTypes {
 
-    public static void Main() {
-        System.out.println( "Hello World!");
+    public static void main(String[] args) {
+        int age;
+        int test1, test2, test3;
+
+        age = 42;
+        test3 = 97;
+        int numOfSubs = 4;
+
+        double weight, heightCm, gpa;
+
+        weight = 152.7;
+        gpa = 4.1;
+        double reallyBig = 1.2345E23;
+
+        char firstInitial = 'D';
+
+        char favSymbol = 'e';
+
+        boolean test = false;
+        boolean isThisTrue = 13 > 45;
+        test1 = 4;
+        boolean isTheSame = numOfSubs == test1;
+        System.out.println(isTheSame);
+
     }
 
 }
