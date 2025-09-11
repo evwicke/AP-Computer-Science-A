e8587d81edb670dd54854c22936fde4b876d9e5d
evwicke <evanwicke@icloud.com>
2025-09-11 10:28:54 -0400

    update coin calculation logic in Project5.java for better output formatting and dynamic totalCents handling

---
+++ In Class Assignments/src/OperationAndNewDataTypes.java
@@ -0,0 +1,17 @@
+/*
+
+        Author : Evan Wicke
+        Date : 9/11/2025
+        Purpose: Getting more comfortable with the use of data types and using the compiler.
+
+ */
+
+import java.util.Scanner;
+
+public class OperationAndNewDataTypes {
+
+    public static void Main() {
+        System.out.println( "Hello World!");
+    }
+
+}
