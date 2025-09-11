dad648853e136a15f8597cc34e8264de1a056845
evwicke <evanwicke@icloud.com>
2025-09-11 10:05:28 -0400

    update coin calculation logic in Project5.java for better output formatting and dynamic totalCents handling

--- In Class Assignments/src/Project5.java
+++ In Class Assignments/src/Project5.java
@@ -1,19 +1,21 @@
 
 public class Project5 {
     public static void main(String[] args) {
-        int totalCents = 165;
+        int totalCents = 821;
         int[] coinWorth = {25, 10, 5, 1};
         String[] coinName = {"Quarter", "Dime", "Nickel", "Penny"};
         int coinAmtBuffer;
 
+        System.out.println("Your amount of change is: " + totalCents + " cents" + "\n" +
+                "This is equivalent to:");
+
         for(int i = 0; i<=3; i++){
-            coinAmtBuffer = 0;
-
-            coinAmtBuffer = totalCents / coinWorth[i];
-            System.out.println(coinAmtBuffer + " " + coinName[i] +
-                    ((coinAmtBuffer >1 || coinAmtBuffer==0) ? "s" : ""));
-
-            totalCents = totalCents % coinWorth[i];
+            coinAmtBuffer = 0; //resetting the buffer
+            coinAmtBuffer = totalCents / coinWorth[i]; //calculating the amount of coins needed
+            System.out.print((coinAmtBuffer>0)?(coinAmtBuffer + " " + coinName[i] +
+                    ((coinAmtBuffer >1 || coinAmtBuffer==0) ? "s" + "\n": "")): "\b");
+            //logic to see what to print out
+            totalCents = totalCents % coinWorth[i]; //taking away the coins from the total
         }
     }
 }
