33e1ba5e826db43e5e0f7bd4a2b312ece60cdc65
evwicke <evanwicke@icloud.com>
2025-09-12 10:43:19 -0400

    update: modify `OperationAndNewDataTypes.java` to adjust `firstInitial` value, comment out unused print statements, and add rounding examples with `Math` functions

--- In Class Assignments/src/OperationAndNewDataTypes.java
+++ In Class Assignments/src/OperationAndNewDataTypes.java
@@ -22,7 +22,7 @@ public class OperationAndNewDataTypes {
         gpa = 4.1;
         double reallyBig = 1.2345E23;
 
-        char firstInitial = 'D';
+        char firstInitial = 'E';
 
         char favSymbol = 'e';
 
@@ -30,7 +30,36 @@ public class OperationAndNewDataTypes {
         boolean isThisTrue = 13 > 45;
         test1 = 4;
         boolean isTheSame = numOfSubs == test1;
-        System.out.println(isTheSame);
+        // System.out.println(isTheSame);
+        // System.out.println(test1 != numOfSubs);
+        // System.out.println(5 == 8 || 7 < 10);
+
+        //System.out.println(firstInitial + " is " + (int)firstInitial + " in ASCII.");
+
+        //create the ASCII chart
+
+//        for (int i = 1; i <= 128; i++) {
+//            System.out.print(i + " - " + (char)i + "\t    " + /* if divisible by 8, go to next line */ ((i%8 == 0)  ?  "\n"  :  "" ));
+//
+//        }
+
+        //DO NOT USE MATH.ROUND.
+        // abs, pow, sqrt, random
+
+        double randNum = Math.random();
+        double num1 = 1 * randNum;
+        double num2 = -1 * num1;
+        System.out.println(randNum);
+
+        int nearestPosInt = (int)(num1 + 0.5);
+        System.out.println(num1 + " rounds to " + nearestPosInt);
+
+        int nearestNegInt = (int)(num2 - 0.5);
+        System.out.println(num2 + " rounds to " + nearestNegInt);
+
+        double num1Round3 = ((int)((num1 * 1000.0)+.5))/1000.0;
+
+        System.out.println(num1Round3);
 
     }
 
