06f7555fcb9f8482173ababa5f09c0ba8a933e07
evwicke <evanwicke@icloud.com>
2025-09-16 10:43:14 -0400

    update: add character manipulation, string operations, and constants usage examples in `OperationAndNewDataTypes.java`

--- In Class Assignments/src/OperationAndNewDataTypes.java
+++ In Class Assignments/src/OperationAndNewDataTypes.java
@@ -6,6 +6,8 @@
 
  */
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class OperationAndNewDataTypes {
 
     public static void main(String[] args) {
@@ -46,20 +48,82 @@ public class OperationAndNewDataTypes {
         //DO NOT USE MATH.ROUND.
         // abs, pow, sqrt, random
 
-        double randNum = Math.random();
-        double num1 = 1 * randNum;
-        double num2 = -1 * num1;
-        System.out.println(randNum);
+//        double randNum = Math.random();
+//        double num1 = 1 * randNum;
+//        double num2 = -1 * num1;
+//        System.out.println(randNum);
+//
+//        int nearestPosInt = (int)(num1 + 0.5);
+//        System.out.println(num1 + " rounds to " + nearestPosInt);
+//
+//        int nearestNegInt = (int)(num2 - 0.5);
+//        System.out.println(num2 + " rounds to " + nearestNegInt);
+//
+//        double num1Round3 = ((int)((num1 * 1000.0)+.5))/1000.0;
+//
+//        System.out.println(num1Round3);
+//
+//        Scanner scanny = new Scanner(System.in);
+//        System.out.print("Enter grade 1: ");
+//        int grade1 = scanny.nextInt();
+//        System.out.println("Your first test score: " + grade1);
+//        // CONSUME THE ENTER KEY!!!
+//        // WHEN YOU CHANGE THE DATA TYPE OF A VARIABLE. (specifically from a number to a string)
+//        scanny.nextLine();
+//
+//
+//        System.out.print("Enter your hometown: ");
+//        String hometown = scanny.nextLine();
+//        System.out.println("Your hometown is " + hometown);
+//
+//        System.out.println("Enter your GPA");
+//        double gpab = scanny.nextDouble();
+//        System.out.println("Your GPA is " + gpab);
+//
+//
+//        scanny.nextLine(); //to consume the enter key.
+//        System.out.println("Enter a character: ");
+//        char letter = scanny.nextLine().charAt(0);
+//        System.out.println("The letter you entered is " + letter + ".");
 
-        int nearestPosInt = (int)(num1 + 0.5);
-        System.out.println(num1 + " rounds to " + nearestPosInt);
+        // chars
 
-        int nearestNegInt = (int)(num2 - 0.5);
-        System.out.println(num2 + " rounds to " + nearestNegInt);
+        String favSaying = "What the sigma!";
+        char firstChar = favSaying.charAt(0);
+        char lastChar = favSaying.charAt(favSaying.length()-1);
 
-        double num1Round3 = ((int)((num1 * 1000.0)+.5))/1000.0;
+        //without the -1 there is an IOUB error.
 
-        System.out.println(num1Round3);
+        System.out.println(firstChar + " " + lastChar);
+
+        //JOIN
+
+        String f = "Evan", l = "Wicke";
+
+        String join = f + " " + l;
+        System.out.println(join + 2 + 5);
+        System.out.println(join + (2 + 5));
+        System.out.println(5 + 2 + join);
+        System.out.println(24 + (.5*4*24) - (.5*36*6));
+
+        // |----------------------------------------SUBSTRINGS----------------------------------------------|
+        //substring with a int gives the index of the int and prints the rest of the string.
+        //substring with an int and an int gives the index of the int until the index of the second int.
+        String testString = "Should've would've could've";
+
+        System.out.println(testString.substring(3,7));
+        System.out.println(testString.substring(0,5));
+        // first number in the range is inclusive, second is exclusive. for example in range notation -> [x,y)
+
+        // .toUpperCase() and .toLowerCase() //recall final (CONSTANTS)
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        final double PI = Math.PI;
+        System.out.println(PI);
+
+        System.out.println(testString.toUpperCase());
+
+
 
     }
 
