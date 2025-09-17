709c110991b1296168cf91de6c8d594cdf4e4ce2
evwicke <evanwicke@icloud.com>
2025-09-17 10:44:57 -0400

    update: comment out all code in `OperationAndNewDataTypes.java` for future refinement and remove unused variable declarations

--- In Class Assignments/SyntaxNOperations/src/OperationAndNewDataTypes.java
+++ In Class Assignments/SyntaxNOperations/src/OperationAndNewDataTypes.java
@@ -9,120 +9,118 @@
 public class OperationAndNewDataTypes {
 
     public static void main(String[] args) {
-        int age;
-        int test1, test2, test3;
-
-        age = 42;
-        test3 = 97;
-        int numOfSubs = 4;
-
-        double weight, heightCm, gpa;
-
-        weight = 152.7;
-        gpa = 4.1;
-        double reallyBig = 1.2345E23;
-
-        char firstInitial = 'E';
-
-        char favSymbol = 'e';
-
-        boolean test = false;
-        boolean isThisTrue = 13 > 45;
-        test1 = 4;
-        boolean isTheSame = numOfSubs == test1;
-        // System.out.println(isTheSame);
-        // System.out.println(test1 != numOfSubs);
-        // System.out.println(5 == 8 || 7 < 10);
-
-        //System.out.println(firstInitial + " is " + (int)firstInitial + " in ASCII.");
-
-        //create the ASCII chart
-
-//        for (int i = 1; i <= 128; i++) {
-//            System.out.print(i + " - " + (char)i + "\t    " + /* if divisible by 8, go to next line */ ((i%8 == 0)  ?  "\n"  :  "" ));
+//        int age;
+//        int test1, test2, test3;
 //
-//        }
-
-        //DO NOT USE MATH.ROUND.
-        // abs, pow, sqrt, random
-
-//        double randNum = Math.random();
-//        double num1 = 1 * randNum;
-//        double num2 = -1 * num1;
-//        System.out.println(randNum);
+//        age = 42;
+//        test3 = 97;
+//        int numOfSubs = 4;
 //
-//        int nearestPosInt = (int)(num1 + 0.5);
-//        System.out.println(num1 + " rounds to " + nearestPosInt);
+//        double weight, heightCm, gpa;
 //
-//        int nearestNegInt = (int)(num2 - 0.5);
-//        System.out.println(num2 + " rounds to " + nearestNegInt);
+//        weight = 152.7;
+//        gpa = 4.1;
+//        double reallyBig = 1.2345E23;
 //
-//        double num1Round3 = ((int)((num1 * 1000.0)+.5))/1000.0;
+//        char firstInitial = 'E';
 //
-//        System.out.println(num1Round3);
+//        char favSymbol = 'e';
 //
-//        Scanner scanny = new Scanner(System.in);
-//        System.out.print("Enter grade 1: ");
-//        int grade1 = scanny.nextInt();
-//        System.out.println("Your first test score: " + grade1);
-//        // CONSUME THE ENTER KEY!!!
-//        // WHEN YOU CHANGE THE DATA TYPE OF A VARIABLE. (specifically from a number to a string)
-//        scanny.nextLine();
+//        boolean test = false;
+//        boolean isThisTrue = 13 > 45;
+//        test1 = 4;
+//        boolean isTheSame = numOfSubs == test1;
+//        // System.out.println(isTheSame);
+//        // System.out.println(test1 != numOfSubs);
+//        // System.out.println(5 == 8 || 7 < 10);
 //
+//        //System.out.println(firstInitial + " is " + (int)firstInitial + " in ASCII.");
 //
-//        System.out.print("Enter your hometown: ");
-//        String hometown = scanny.nextLine();
-//        System.out.println("Your hometown is " + hometown);
+//        //create the ASCII chart
 //
-//        System.out.println("Enter your GPA");
-//        double gpab = scanny.nextDouble();
-//        System.out.println("Your GPA is " + gpab);
+////        for (int i = 1; i <= 128; i++) {
+////            System.out.print(i + " - " + (char)i + "\t    " + /* if divisible by 8, go to next line */ ((i%8 == 0)  ?  "\n"  :  "" ));
+////
+////        }
 //
+//        //DO NOT USE MATH.ROUND.
+//        // abs, pow, sqrt, random
 //
-//        scanny.nextLine(); //to consume the enter key.
-//        System.out.println("Enter a character: ");
-//        char letter = scanny.nextLine().charAt(0);
-//        System.out.println("The letter you entered is " + letter + ".");
-
-        // chars
-
-        String favSaying = "What the sigma!";
-        char firstChar = favSaying.charAt(0);
-        char lastChar = favSaying.charAt(favSaying.length()-1);
-
-        //without the -1 there is an IOUB error.
-
-        System.out.println(firstChar + " " + lastChar);
-
-        //JOIN
-
-        String f = "Evan", l = "Wicke";
-
-        String join = f + " " + l;
-        System.out.println(join + 2 + 5);
-        System.out.println(join + (2 + 5));
-        System.out.println(5 + 2 + join);
-        System.out.println(24 + (.5*4*24) - (.5*36*6));
-
-        // |----------------------------------------SUBSTRINGS----------------------------------------------|
-        //substring with a int gives the index of the int and prints the rest of the string.
-        //substring with an int and an int gives the index of the int until the index of the second int.
-        String testString = "Should've would've could've";
-
-        System.out.println(testString.substring(3,7));
-        System.out.println(testString.substring(0,5));
-        // first number in the range is inclusive, second is exclusive. for example in range notation -> [x,y)
-
-        // .toUpperCase() and .toLowerCase() //recall final (CONSTANTS)
-        System.out.println(Integer.MIN_VALUE);
-        System.out.println(Integer.MAX_VALUE);
-        final double PI = Math.PI;
-        System.out.println(PI);
-
-        System.out.println(testString.toUpperCase());
-
-
-
+////        double randNum = Math.random();
+////        double num1 = 1 * randNum;
+////        double num2 = -1 * num1;
+////        System.out.println(randNum);
+////
+////        int nearestPosInt = (int)(num1 + 0.5);
+////        System.out.println(num1 + " rounds to " + nearestPosInt);
+////
+////        int nearestNegInt = (int)(num2 - 0.5);
+////        System.out.println(num2 + " rounds to " + nearestNegInt);
+////
+////        double num1Round3 = ((int)((num1 * 1000.0)+.5))/1000.0;
+////
+////        System.out.println(num1Round3);
+////
+////        Scanner scanny = new Scanner(System.in);
+////        System.out.print("Enter grade 1: ");
+////        int grade1 = scanny.nextInt();
+////        System.out.println("Your first test score: " + grade1);
+////        // CONSUME THE ENTER KEY!!!
+////        // WHEN YOU CHANGE THE DATA TYPE OF A VARIABLE. (specifically from a number to a string)
+////        scanny.nextLine();
+////
+////
+////        System.out.print("Enter your hometown: ");
+////        String hometown = scanny.nextLine();
+////        System.out.println("Your hometown is " + hometown);
+////
+////        System.out.println("Enter your GPA");
+////        double gpab = scanny.nextDouble();
+////        System.out.println("Your GPA is " + gpab);
+////
+////
+////        scanny.nextLine(); //to consume the enter key.
+////        System.out.println("Enter a character: ");
+////        char letter = scanny.nextLine().charAt(0);
+////        System.out.println("The letter you entered is " + letter + ".");
+//
+//        // chars
+//
+//        String favSaying = "What the sigma!";
+//        char firstChar = favSaying.charAt(0);
+//        char lastChar = favSaying.charAt(favSaying.length()-1);
+//
+//        //without the -1 there is an IOUB error.
+//
+//        System.out.println(firstChar + " " + lastChar);
+//
+//        //JOIN
+//
+//        String f = "Evan", l = "Wicke";
+//
+//        String join = f + " " + l;
+//        System.out.println(join + 2 + 5);
+//        System.out.println(join + (2 + 5));
+//        System.out.println(5 + 2 + join);
+//        System.out.println(24 + (.5*4*24) - (.5*36*6));
+//
+//        // |----------------------------------------SUBSTRINGS----------------------------------------------|
+//        //substring with a int gives the index of the int and prints the rest of the string.
+//        //substring with an int and an int gives the index of the int until the index of the second int.
+//        String testString = "Should've would've could've";
+//
+//        System.out.println(testString.substring(3,7));
+//        System.out.println(testString.substring(0,5));
+//        // first number in the range is inclusive, second is exclusive. for example in range notation -> [x,y)
+//
+//        // .toUpperCase() and .toLowerCase() //recall final (CONSTANTS)
+//        System.out.println(Integer.MIN_VALUE);
+//        System.out.println(Integer.MAX_VALUE);
+//        final double PI = Math.PI;
+//        System.out.println(PI);
+//
+//        System.out.println(testString.toUpperCase());
+//
     }
 
 }
