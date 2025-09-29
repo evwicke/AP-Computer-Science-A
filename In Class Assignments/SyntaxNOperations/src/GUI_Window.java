5b0a4fcaec4427a16725ac741e8f6e391c3d6519
Evan Wicke 1011509 <1011509@student.crsd.org>
2025-09-29 10:43:27 -0400

    yes

---
+++ In Class Assignments/SyntaxNOperations/src/GUI_Window.java
@@ -0,0 +1,96 @@
+package SyntaxNOperations.src;
+
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+import javax.swing.*; // for jpanels and jframes
+import java.awt.*; // for colors and containers
+
+public class GUI_Window{
+    public static void main() throws InterruptedException { 
+        final int WIDTH = 300;
+        final int HEIGHT = 500;
+
+        // Instantiation ... making an instance of an object.
+        // ClassName identifier = new ClassName(args[]);
+
+        JFrame Frame = new JFrame();
+        Frame.setTitle("First GUI Program");
+        Frame.setSize(WIDTH, HEIGHT);
+        Frame.setLocation(100,50);
+        Frame.setBackground(Color.black);
+        Frame.setResizable(true);
+        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Basic color panels
+        JPanel Panel1 = new JPanel();
+        Panel1.setBackground(Color.pink);
+        JPanel Panel2 = new JPanel();
+        Panel2.setBackground(Color.green);
+        JPanel Panel3 = new JPanel();
+        Panel3.setBackground(Color.blue);
+        JPanel Panel4 = new JPanel();
+        Panel4.setBackground(Color.red);
+        JPanel Panel5 = new JPanel();
+        Panel5.setBackground(Color.yellow);
+
+        // New color via RGB
+        JPanel PanelRGB = new JPanel();
+        Color newColor = new Color(100,42,58);
+        PanelRGB.setBackground(newColor);
+
+        // Attach the panels to the frame.
+        // Grid layout.
+        GridLayout Grid = new GridLayout(3,4);
+        Frame.setLayout(Grid);
+        Frame.add(Panel1);
+        Frame.add(Panel2);
+        Frame.add(Panel3);
+        Frame.add(Panel4);
+        Frame.add(Panel5);
+        Frame.add(PanelRGB);
+
+        // BorderLayout
+        BorderLayout Border = new BorderLayout();
+        Frame.setLayout(Border);
+        Frame.add(PanelRGB, BorderLayout.WEST); 
+        Frame.add(Panel4, BorderLayout.NORTH);
+        Frame.add(Panel2, BorderLayout.EAST);
+        Frame.add(Panel3, BorderLayout.SOUTH);
+        Frame.add(Panel1, BorderLayout.CENTER);
+
+        //wait  
+        Thread.sleep(1000);
+        Frame.add(Panel5);
+        
+        // Container
+        Container Center = new Container();
+        Center.setLayout(new GridLayout(2,3));
+        
+        Center.add(Panel2);
+        Center.add(Panel3);
+        Center.add(Panel4);
+        Center.add(Panel5);
+        Center.add(PanelRGB);
+        Center.add(Panel1);
+        Frame.add(Center, BorderLayout.CENTER);
+        Frame.add(PanelRGB, BorderLayout.WEST); 
+        Frame.add(Panel4, BorderLayout.NORTH);
+        Frame.add(Panel2, BorderLayout.EAST);
+        Frame.add(Panel3, BorderLayout.SOUTH);
+        
+        Frame.setVisible(true);
+    }
+
+    
+}
---
+++ In Class Assignments/SyntaxNOperations/src/newPanel.java
@@ -0,0 +1,27 @@
+package SyntaxNOperations.src;
+
+
+/**
+ * Write a description of class newPanel here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import javax.swing.*; // for jpanels and jframes
+import java.awt.*; // for colors and containers
+
+public class newPanel
+{
+    /*
+     * Making a constructor, Used in OOP. Need this when you create your own class.
+     * It basically initializes the object when it is created in the main method or the driver class.
+     */
+    public newPanel(Color MyColor){
+        this.setBackground(myColor);
+        
+        
+        
+    }
+    
+}
