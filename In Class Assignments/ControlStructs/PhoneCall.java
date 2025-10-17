ff8646cd6b4e160ffcf9f8515ce327d80fbd9bb4
Evan Wicke 1011509 <1011509@student.crsd.org>
2025-10-17 10:43:01 -0400

    Phone call project

---
+++ In Class Assignments/ControlStructs/PhoneCall.java
@@ -0,0 +1,42 @@
+
+/**
+ * A 2-minute (or less) telephone call to Lexington, Virginia, costs $1.15. 
+ * Each additional minute costs $0.50. Write a program called PhoneCall.java that 
+ * takes the total length of a call in full minutes as input and calculates and 
+ * displays the cost in a nice and clean way that represents money.  
+ * Remember to use proper data types and print meaningful descriptive output. 
+
+ * Optional:  If you want the user to enter floating point types for the length 
+ * of the phone call, you need to account for the fact that a 5 min : 01 sec phone call 
+ * is going to cost the same as a 6 min phone call. 
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class PhoneCall
+{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+       
+        System.out.print("Input the amount of minutes you spoke for: ");
+        double minutes = scan.nextDouble();
+        while(minutes < 0){ // makes it so they cant input a negative number
+            System.out.print("  Input valid time: ");
+            minutes = scan.nextDouble(); // re-prompt
+        }
+        
+        int minutesCeil = (int)(Math.ceil(minutes)); // gets the ceiling of the number.
+        double cost = round(0.5*(minutesCeil - 2 ) + 1.15, 2); // y = mx + b
+        
+        System.out.printf("The cost of this phone call is $%.2f",cost);// MR LYNCH I PROMISE THAT I AM NOT USING
+                                                // THE PRINTF FOR ACTUAL ROUNDING. JUST REMOVING THE TRAILING
+                                                // ZEROS. THE ROUNDING METHOD WORKS TO ROUND THE NUMBERS.
+    }
+    
+    public static double round(double value, int places) { // rounding function outpuyttng a double.
+        // Multiply by 10^places, cast to int, then divide back
+        return (((int) (value * (Math.pow(10, places)) + 0.5)) / (Math.pow(10, places)));
+    }
+}
---
+++ In Class Assignments/ControlStructs/forLoopDemo.java
@@ -0,0 +1,107 @@
+/**
+ * Example of a for loop
+ *  Includes the IsItPrime method that check to
+ *          see if a number is prime or not
+ * 
+ * @dlynch1   
+ * 
+ * @October
+ */
+import java.util.Scanner;
+public class forLoopDemo
+{
+    public static void Main()
+    {
+      
+        System.out.println("**************** example 1  ****************");
+
+        for(int i = 0; i<10; i++){
+            System.out.print(i + " ");
+        }
+        
+        
+        System.out.println();
+        System.out.println("**************** example 2  ****************");
+        //print the odd numbers from 1 to 10
+        for (int i = 1; i<11; i +=2){
+            System.out.print(i + " ");
+        }
+        
+        System.out.println();
+        System.out.println();
+        //print the odd number from 1 to 10 using %
+        for(int i = 1; i<=10; i++){
+            if(i % 2 == 1){
+                System.out.print(i + " ");
+            }
+        }
+        
+        
+        System.out.println();
+        System.out.println("**************** example 3  ****************");
+        // prints 10 to 1
+       
+        
+        
+        System.out.println();
+        System.out.println("**************** example 4  ****************");
+        // print numbers from 37 to 192 that are multiples of 13 & even
+        int start = 37;
+        int finish = 192;
+        //code
+        for (int i = start; i<=finish; i++){
+            if(i % 13 == 0 && i % 2 == 0){
+                System.out.print(i + " ");
+            }
+        }
+        
+
+
+        System.out.println();
+        System.out.println("**************** example 5  ****************");
+        /// finding primes between two numbers
+        //  ask the start and end number the view the primes in between
+        //   and count how many primes are between those numbers
+        Scanner scanny = new Scanner(System.in);
+        System.out.print("Please enter a integer:");
+        int begin = scanny.nextInt();
+        System.out.print("Please enter a bigger integer:");
+        int end = scanny.nextInt();
+        // error loop check to see if a bigger number was entered
+        while(begin >= end)
+        {
+            System.out.print("Please enter a number that is bigger than " 
+                                            + begin + ": ");
+            end = scanny.nextInt();
+        }
+        int count = 0;
+        
+        for(int i = begin; i<= end; i++){
+            if(isPrime(i)){
+                System.out.print(i + "  ");
+                count++;
+            }
+        }
+        
+        
+        
+        
+        System.out.println("\nNumber of primes was: " + count);
+
+
+    }
+    
+    public static boolean isPrime(int num){
+        boolean isPrime = true; // assume its prime.
+        if (num == 1) isPrime = false;
+        for(int i = 2; i< num; i++){
+            if (num % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+   
+    
+}
