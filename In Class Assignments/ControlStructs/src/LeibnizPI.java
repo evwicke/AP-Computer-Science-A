ff026142e8c136ec6a22f6d4dc184bba94416114
evwicke <evanwicke@icloud.com>
2025-10-23 14:09:28 -0400

    feat: Implement Leibniz Pi approximation with error calculation
    This commit introduces a program that approximates the value of Pi using the Leibniz formula.
    
    The program prompts the user for a number of iterations to determine the accuracy of the calculation. It then iteratively computes the sum.
    
    A helper method, `percentageToPI`, has been added to calculate the percent error of the approximation when compared to the value of `Math.PI`. The final output is formatted to clearly display both the calculated approximation and its percent error.

--- In Class Assignments/ControlStructs/src/LeibnizPI.java
+++ In Class Assignments/ControlStructs/src/LeibnizPI.java
@@ -1,11 +1,10 @@
-package src;
+package ControlStructs.src;
 
 
 /**
- * Write a description of class LeibnizPI here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Approximates PI to a certain accuracy via iterations over Leibniz's summation formula for PI
+ * @evanwicke
+ * @10/23/2025
  */
 import java.util.*;
 public class LeibnizPI{
@@ -15,21 +14,30 @@ public class LeibnizPI{
         System.out.print("Please input the iteration accuracy \n  : ");
         int iterations = scan.nextInt();
         
-        long sum = 0;
-        int denominator = 1;
+        double sum = 0;
+        double denominator = 1;
+
+        /* This for loop is a lot easier understood via mathematical functions.
+                                                        ( 4 )
+        (summation from i=1 to i=iterations) of ---------------------
+                                                 (2(i)-1)*(-1)^(i+1)
+         */
+
         for(int i = 1; i<=iterations; i++){
-            
             denominator = (2*i)-1;
             if(i % 2 == 0){
                 denominator *=-1;
             }
-            
-            sum += (long)(4.0/(double)denominator);
-            
-            System.out.println(sum);
+            sum += (4.0/denominator);
         }
-        
-        System.out.println(sum);
-        
+
+        System.out.println();
+        System.out.printf("  Approximation: %f\n  PercentError: %.8f%%\n", sum, percentageToPI(sum));
+
+        scan.close();
+    }
+
+    public static double percentageToPI(double actual){
+        return Math.abs(((actual - Math.PI)/Math.PI) * 100);
     }
 }
