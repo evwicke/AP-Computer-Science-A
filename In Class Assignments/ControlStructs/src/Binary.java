88c651a40eea6a62ace4708284d5b54934c8683b
evwicke <evanwicke@icloud.com>
2025-10-27 09:17:00 -0400

    Refactor `Binary` class to replace do-while with while loop, add user guidance for exiting, and transition to `long` for larger calculations. Update BlueJ metadata for layout improvements and added new targets.

--- In Class Assignments/ControlStructs/src/Binary.java
+++ In Class Assignments/ControlStructs/src/Binary.java
@@ -1,11 +1,11 @@
-package src;
+package ControlStructs.src;
 
 
 /**
- * Write a description of class Binary here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Outputs the powers of 2 based on input from the user.
+ * Uses while loop to ask as many times as the user wants, can be exited by inputting (-1)
+ * @evanwicke
+ * @10/24/2025
  */
 import java.util.*;
 public class Binary
@@ -13,18 +13,24 @@ public class Binary
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         
-        int exp; //initiate exponent variable before loop so that the loop can see it in its scope.
+        int exp = 0; //initiate exponent variable before loop so that the loop can see it in its scope.
         System.out.print("This program will display the powers of 2.\n"
-                +"Note: Enter -1 to halt the program."); 
-        do{
+                +"Note: Enter -1 to halt the program.");
+
+        while(exp != -1){  // while the input isn't -1, keep running.
             System.out.print("\n  Please enter a positive exponent: ");
             exp = scan.nextInt();
-            int answer = 1; // 2^0 = 1, 2^1 = 2, ... etc
+
+            long answer = 1; // 2^0 = 1, 2^1 = 2, ... etc
             for(int i = 1; i<=exp; i++){
                 answer *=2;
             }
-            System.out.printf("  2^%d is %d.", exp, answer); //prints answer
-        }while(exp != -1); // while the input isnt -1, keep running.
+            if(exp!=-1){
+                System.out.printf("  2^%d is %d.", exp, answer); //prints answer
+            }else{
+                System.out.println("Goodbye");
+            }
+        }
         
     }
 }
