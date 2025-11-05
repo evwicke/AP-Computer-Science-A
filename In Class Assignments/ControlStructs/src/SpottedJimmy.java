3f1adb3f7a30a6881aadafd72fc8135f26649e09
Evan Wicke 1011509 <1011509@student.crsd.org>
2025-11-05 10:43:37 -0500

    11/5

---
+++ In Class Assignments/ControlStructs/src/SpottedJimmy.java
@@ -0,0 +1,68 @@
+package src;
+
+
+/**
+ * Write a description of class SpottedJimmy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+public class SpottedJimmy{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+    
+        ArrayList<Integer> catNumList = new ArrayList<Integer>(); //init all of my varibles so they can be changed in the while loop
+        char runner;
+        String runnerFlip;
+        int enemyCount;
+        do{
+            // Clear the catNumList every time ( used for when the user reruns the code )
+            catNumList.clear();
+            
+            print("Input how many enemies you have: ");
+            enemyCount = scan.nextInt();
+            for(int i = 1; i<=enemyCount;i++){
+                print("  How many cats does enemy " + i + " have?\n   : ");
+                catNumList.add(scan.nextInt());
+            }
+            print("\n" + catNumList);
+            
+            int catSum = catNumList.stream().mapToInt(Integer::intValue).sum(); // get the sum of the array
+        
+            int oddCatAmounts = 0;
+            for(int cat : catNumList){
+                if(cat % 2 == 1){
+                    oddCatAmounts++;
+                }
+            }
+            int catGreater17Div7 = 0;
+            for(int cat : catNumList){
+                if(cat > 17 && cat % 7 == 0){
+                    catGreater17Div7++;
+                }
+            }
+            System.out.printf("\n Total Amt of Cats: %d\n"
+                        + "\n Odd Cat Amts: %d\n"
+                        + "\n Cat Amts > 17 and divisible by 7: %d\n",
+                        catSum, oddCatAmounts, catGreater17Div7
+            );
+            
+            
+            //logic for rerunning
+            scan.nextLine(); // CLEAR THE ENTER KEY!!
+            print("\n Rerun? (y/n)");
+            runner = (char)scan.next().charAt(0); // using to just get the first word the user enters
+            runnerFlip = String.valueOf(runner); // turns the char into a string
+        }while(runnerFlip.equalsIgnoreCase("y"));
+        
+        
+       
+        
+    }
+    
+    public static void print(String arg){
+        System.out.print(arg);
+    }
+}
