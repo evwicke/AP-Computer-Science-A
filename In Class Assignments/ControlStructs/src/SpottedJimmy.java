4dedf8aabe752bb15322315245407853c1fb626b
Evan Wicke 1011509 <1011509@student.crsd.org>
2025-11-05 14:41:59 -0500

    clinic 11/5

--- In Class Assignments/ControlStructs/src/SpottedJimmy.java
+++ In Class Assignments/ControlStructs/src/SpottedJimmy.java
@@ -17,18 +17,26 @@ public class SpottedJimmy{
         char runner;
         String runnerFlip;
         int enemyCount;
+		int runCounter = 0;
         do{
             // Clear the catNumList every time ( used for when the user reruns the code )
             catNumList.clear();
-            
+
+			if(runCounter != 0){
+				System.out.println("----------------------------------------------");
+			 }
+
+            //user input
             print("Input how many enemies you have: ");
             enemyCount = scan.nextInt();
             for(int i = 1; i<=enemyCount;i++){
                 print("  How many cats does enemy " + i + " have?\n   : ");
                 catNumList.add(scan.nextInt());
             }
-            print("\n" + catNumList);
             
+
+
+			// analysis
             int catSum = catNumList.stream().mapToInt(Integer::intValue).sum(); // get the sum of the array
         
             int oddCatAmounts = 0;
@@ -43,8 +51,10 @@ public class SpottedJimmy{
                     catGreater17Div7++;
                 }
             }
-            System.out.printf("\n Total Amt of Cats: %d\n"
-                        + "\n Odd Cat Amts: %d\n"
+
+			//outputting and formatting response
+            System.out.printf("\n Total Amt of Cats: %d"
+                        + "\n Odd Cat Amts: %d"
                         + "\n Cat Amts > 17 and divisible by 7: %d\n",
                         catSum, oddCatAmounts, catGreater17Div7
             );
@@ -55,11 +65,8 @@ public class SpottedJimmy{
             print("\n Rerun? (y/n)");
             runner = (char)scan.next().charAt(0); // using to just get the first word the user enters
             runnerFlip = String.valueOf(runner); // turns the char into a string
+			runCounter++;
         }while(runnerFlip.equalsIgnoreCase("y"));
-        
-        
-       
-        
     }
     
     public static void print(String arg){
