f73fe9e9aa3ea7fc6a1d701a1e69056524d13756
evwicke <evanwicke@icloud.com>
2025-11-18 10:05:31 -0500

    Remove `Circle` and `CircleClient` classes along with related metadata. Update BlueJ layout and dependencies. Enhance `PingPong` for better win tracking and statistics calculation logic.

--- In Class Assignments/ControlStructs/src/PingPong.java
+++ In Class Assignments/ControlStructs/src/PingPong.java
@@ -2,10 +2,10 @@ package src;
 
 
 /**
- * Write a description of class PingPong here.
+ * ping pong wins.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @evwicke
+ * @11.18.25
  */
 import java.util.*;
 import java.util.function.*;
@@ -15,23 +15,43 @@ public class PingPong{
         Scanner scan = new Scanner(System.in);
         
         Predicate<Integer> isInRange = d -> d>=5;
-        int playerCount = InputSentinel.sentinel(Integer::parseInt , isInRange , "Please input the amount of players (minimum 5: ");
+        int playerCount = InputSentinel.sentinel(Integer::parseInt , isInRange , "Please input the amount of players (minimum 5): ");
         // the InputSentinel Class is submitted too.
         ArrayList<Integer> winsArray = new ArrayList<Integer>(playerCount);
         for(int i = 1; i<=playerCount;i++){
             Predicate<Integer> isValidCountingNumber = d -> d>=0;
             winsArray.add(InputSentinel.sentinel(Integer::parseInt, isValidCountingNumber,
                                                         "Win count of player " + i + ": "));
-            
-            
+            if(winsArray.stream().mapToInt(Integer::intValue).sum() >= 50){
+                playerCount = winsArray.size();
+                System.out.println("OVERLOAD");
+                break;
+            }
         }
-        
-        System.out.println(winsArray);
-        System.out.println(playerCount);
-        
-        
-        
-        
-        
+
+        int winSum = 0;
+        for(int i = 0; i<playerCount;i++){
+            winSum += winsArray.get(i);
+        }
+
+        double averageWins = (double) winSum / playerCount;
+
+        int mostWins = winsArray.stream().max(Integer::compareTo).get();
+        int leastWins = winsArray.stream().min(Integer::compareTo).get();
+
+        int multiDigitWins = 0;
+        for(int i = 0; i<playerCount;i++){
+            if(winsArray.get(i)>9){
+                multiDigitWins++;
+            }
+        }
+
+        System.out.printf("\n The average number of wins is: %.2f " +
+                "\n The most wins is: %d " +
+                "\n The least wins is: %d " +
+                "\n There are %d players with double digit win counts.",
+                averageWins, mostWins, leastWins, multiDigitWins
+        );
+
     }
 }
