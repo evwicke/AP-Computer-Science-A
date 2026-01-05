0141642e92af662435c3bcb3eba3164b6e6834ff
Evan Wicke 1011509 <1011509@student.crsd.org>
2026-01-05 10:36:19 -0500

    1.5.26

--- In Class Assignments/ControlStructs/src/TextFormatter.java
+++ In Class Assignments/ControlStructs/src/TextFormatter.java
@@ -6,7 +6,7 @@ public class TextFormatter{
     {
         line = lineToFormat;
     }
-    
+
     public int findString(String str, int start){
         char target = str.charAt(0);
         for (int i = start; i < line.length(); i++) {
@@ -20,14 +20,56 @@ public class TextFormatter{
         }
         return -1;
     }
-    
-    public int countStrings(String str)
-    {
-        return -1;
+
+    public int countStrings(String str) {
+        int count = 0;
+        int recentFindIndex = 0;
+        while (recentFindIndex < line.length()) {
+            recentFindIndex = findString(str, recentFindIndex);
+            if (recentFindIndex == -1) {
+                break; // No more matches found, exit the loop
+            }
+            count++;
+            recentFindIndex++;
+        }
+        return count;
     }
 
-    public String convertItalics()
-    {
-        return "";
+    public String convertItalics() {
+        if( countSimple("_") % 2 == 1 ){
+            return line;
+        }
+
+        String[] letters = line.split("");
+        int oscillator = 1;
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i].equals("_")) {
+                if (oscillator == 1) {
+                    letters[i] = "<I>"; 
+                    oscillator *= -1;
+                } 
+                else if (oscillator == -1) {
+                    letters[i] = "</I>";
+                    oscillator *= -1;
+                }
+            }
+        }
+
+        String returner = "";
+        for (String u : letters) {
+            returner += u;
+        }
+
+        return returner;
+    }
+
+    public int countSimple(String str) {
+        int count = 0;
+        for (int i = 0; i <= line.length() - str.length(); i++) {
+            if (line.substring(i, i + str.length()).equals(str)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
--- In Class Assignments/ControlStructs/src/textdisplayer.java
+++ In Class Assignments/ControlStructs/src/textdisplayer.java
@@ -15,5 +15,14 @@ public class textdisplayer
         System.out.println(texty.findString("b",4));
         System.out.println(texty.findString("c",0));
         
+        System.out.println("-----");
+        TextFormatter texty2 = new TextFormatter("_This_ is _very_ _good_.");
+        System.out.println(texty2.countStrings("_"));
+        TextFormatter texty3 = new TextFormatter("This is ___ very _good_.");
+        System.out.println(texty3.countStrings("_"));
+        
+        System.out.println("-----");
+        System.out.println(texty2.convertItalics());
+        System.out.println(texty3.convertItalics());
     }
 }
