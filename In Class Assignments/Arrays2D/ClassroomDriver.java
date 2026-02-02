0110c292e1576cf71f10e6f4fe65d00b8f1e03f5
Evan Wicke 1011509 <1011509@student.crsd.org>
2026-02-02 10:43:34 -0500

    2/2

--- In Class Assignments/Arrays2D/ClassroomDriver.java
+++ In Class Assignments/Arrays2D/ClassroomDriver.java
@@ -2,50 +2,58 @@ import java.util.*;
 public class ClassroomDriver{
     public static void Main(){
         Scanner scan = new Scanner(System.in);
-        
+
         System.out.print("How many rows in your class?\n  : ");
         int rows = scan.nextInt();
         System.out.print("How many columns in your class?\n  : ");
         int cols = scan.nextInt();
-        
+        System.out.print("How many people in your class?\n  : ");
+        int people = scan.nextInt();
+
         Classroom myClass = new Classroom(rows, cols);
-        
+
         clearScreen();
         myClass.printClass();
 
-        askSeat(rows, cols, myClass, scan);
-        clearScreen();
-        myClass.printClass();
+        for(int i = 0; i<people; i++){
+            clearScreen();
+            askSeat(rows, cols, myClass, scan);
+            clearScreen();
+            myClass.printClass();
+        }
     }
 
     public static void clearScreen() {
         System.out.print('\u000c');
     }
     
+    static boolean flip = false;
     public static void askSeat(int rows, int cols, Classroom myClass, Scanner scan){
+        if(flip == true){
+            System.out.println("Seat not available. Try again.");
+            flip = false;
+        }
+
         try{
-            clearScreen();
             myClass.printClass();
-            System.out.println("Which row do you want to sit in? \n  : ");
+            System.out.print("Which row do you want to sit in? \n  : ");
             int row = scan.nextInt();
             System.out.print("Which column do you want to sit in? \n  : ");
             int col = scan.nextInt();
             if(myClass.seatFilled(row, col)){
+                flip = true;
                 askSeat(rows, cols, myClass, scan);
-                System.out.println("Seat taken.");
             }else{
-                System.out.println("What are your initials? \n  : ");
-                scan.next();
+                scan.nextLine();
+                System.out.print("What are your initials? \n  : ");
                 String init = scan.nextLine();
-                
+
                 myClass.setSeat(row, col, init);
             }
         }catch(Exception e){
-            
-            
+            scan.nextLine(); // clear the enter key
+            flip = true;
             askSeat(rows, cols, myClass, scan);
-            System.out.println("Seat out of the classroom.");
-            
         }
     }
 }
